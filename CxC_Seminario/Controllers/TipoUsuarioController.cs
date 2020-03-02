292ff4e714418ab36a34d6eba658a2a48d5e8c42
efuentesr9609 <edu9609@hotmail.com>
2020-03-01 20:55:21 -0600

    Creacion de controlador TipoUsuario en el MVC y las vistas de TipoUsuario

---
+++ CxC_Seminario/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using CxC_Seminario.DO;
+using Newtonsoft.Json;
+
+namespace CxC_Seminario.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        string baseurl = "https://localhost:44313/";
+        // GET: TipoUsuario
+        public async Task<ActionResult> Index()
+        {
+            List<TipoUsuario> aux = new List<TipoUsuario>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/TipoUsuario/GetAll");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxRes = res.Content.ReadAsStringAsync().Result;
+
+                    aux = JsonConvert.DeserializeObject<List<TipoUsuario>>(auxRes);
+                }
+            }
+            return View(aux);
+        }
+        public async Task<ActionResult> Details(int? id)
+        {
+            TipoUsuario aux = new TipoUsuario();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/TipoUsuario/GetOneById/?id= " + id);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxRes = res.Content.ReadAsStringAsync().Result;
+
+                    aux = JsonConvert.DeserializeObject<TipoUsuario>(auxRes);
+                }
+            }
+            return View(aux);
+        }
+        #region Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TipoUsuario entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+
+                var myContent = JsonConvert.SerializeObject(entidad);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var postTask = client.PostAsync("api/TipoUsuario/Insert", byteContent).Result;
+
+                var result = postTask;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server Error, Please contact administrator");
+            return View(entidad);
+        }
+        #endregion
+        #region Edit
+        public async Task<ActionResult> Edit(int? id)
+        {
+            TipoUsuario aux = new TipoUsuario();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/TipoUsuario/GetOneById/?id=" + id);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxRes = res.Content.ReadAsStringAsync().Result;
+
+                    aux = JsonConvert.DeserializeObject<TipoUsuario>(auxRes);
+                }
+            }
+            return View(aux);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TipoUsuario entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+
+                var myContent = JsonConvert.SerializeObject(entidad);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var postTask = client.PostAsync("api/TipoUsuario/Updated", byteContent).Result;
+
+                var result = postTask;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server Error, Please contact administrator");
+            return View(entidad);
+        }
+        #endregion
+        #region Delete
+        public async Task<ActionResult> Delete(int? id)
+        {
+            TipoUsuario aux = new TipoUsuario();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/TipoUsuario/GetOneById/?id=" + id);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxRes = res.Content.ReadAsStringAsync().Result;
+
+                    aux = JsonConvert.DeserializeObject<TipoUsuario>(auxRes);
+                }
+            }
+            return View(aux);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            TipoUsuario aux = new TipoUsuario();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("api/TipoUsuario/GetOneById/?id=" + id);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var auxRes = res.Content.ReadAsStringAsync().Result;
+
+                    aux = JsonConvert.DeserializeObject<TipoUsuario>(auxRes);
+                }
+
+                var myContent = JsonConvert.SerializeObject(aux);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var postTask = client.PostAsync("api/TipoUsuario/Delete", byteContent).Result;
+
+                var result = postTask;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server Error, Please contact administrator");
+            return View(aux);
+        }
+        #endregion
+    }
+}
---
+++ CxC_Seminario/DO/TipoUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CxC_Seminario.DO
+{
+    public class TipoUsuario
+    {
+        public int idTipoUsuario { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
